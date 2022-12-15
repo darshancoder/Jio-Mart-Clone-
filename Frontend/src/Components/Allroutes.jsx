a172c0e174f0e1f7937d2057225d20c5133f20cd
UdayCreative <udaypatil544@gmial.com>
2022-12-15 15:59:19 +0530

    sd

--- Frontend/src/Components/Allroutes.jsx
+++ Frontend/src/Components/Allroutes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Cartpage from './Cartpage';
 import Data from './local';
+import Products from './ProductPage/Products';
 
 function Allroutes(props) {
     return (
         <Routes>
             <Route path='/cartpage' element={<Cartpage/>}/>
             <Route path='/local' element={<Data/>}/>
+            <Route path='/fruits' element={<Products/>}/>
         </Routes>
     );
 }
